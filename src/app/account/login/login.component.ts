720b348de0d6d224239b46a2b4f9213080acedda
User1 <arjun@eamandloi@eworkplaceapps.com>
2019-01-07 18:51:18 +0530

    zoom lvl

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  
+  @ViewChild('scaleImg') scaleImg;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  plus(zoomLevel){
+    this.scaleImg.nativeElement.style.zoom = zoomLevel;
+  }
+ 
 
 }
